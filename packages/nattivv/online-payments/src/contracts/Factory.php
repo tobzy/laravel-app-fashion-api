5cd4c3d6087e993a534cd896820c8a3cee3d8555
Tobechukwu Onuegbu <Tobechukwu.Onuegbu@Interswitchng.com>
2017-03-26 19:52:04 +0100

    Nattivv payments

---
+++ packages/nattivv/online-payments/src/contracts/Factory.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace Nattivv\OnlinePayments\Contracts;
+
+
+interface Factory
+{
+    /**
+     * Get an OAuth provider implementation.
+     *
+     * @param  string  $driver
+     * @return \Laravel\Socialite\Contracts\Provider
+     */
+    public function driver($driver = null);
+}
